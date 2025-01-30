e8e05cb059d780f246e312ff0287cc4188a60d7d
djemoui <34734830+badrdjemoui@users.noreply.github.com>
2025-01-30 21:32:00 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,16 +1,16 @@
 // App.js
-import React from 'react';
-import AddTask from './components/AddTask';
-import TaskList from './components/TaskList';
+import React from 'react'; // Importing React library
+import AddTask from './components/AddTask'; // Importing AddTask component for adding tasks
+import TaskList from './components/TaskList'; // Importing TaskList component for displaying tasks
 
 const App = () => {
   return (
     <div>
-      <h1>Task Manager</h1>
-      <AddTask />
-      <TaskList />
+      <h1>Task Manager</h1> {/* Displaying the main title of the app */}
+      <AddTask /> {/* Rendering the AddTask component to allow users to add tasks */}
+      <TaskList /> {/* Rendering the TaskList component to show the list of tasks */}
     </div>
   );
 };
 
-export default App;
+export default App; // Exporting the App component to be used in the main entry point (index.js)
